dabd7549c9a9f5223445573238952a61d1fa1799
Minh Yến <yen98kma@gmail.com>
2020-06-17 22:16:50 +0700

    Form validation and store

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -21,7 +21,7 @@
                         
                         <div class="form-group">
                             <label for="question-title">Question Title</label>
-                            <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}} ">
+                            <input type="text" name="title" id="question-title" value="{{ old('title') }}" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}} ">
                             @if ($errors->has('title'))
                                 <div class="invalid-feedback">
                                     <strong>{{ $errors->first('title') }}</strong>
@@ -31,7 +31,7 @@
                         
                         <div class="form-group">
                             <label for="question-body">Explan you question</label>
-                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}} "></textarea>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}} ">{{ old('body') }}</textarea>
                             @if ($errors->has('body'))
                                 <div class="invalid-feedback">
                                     <strong>{{ $errors->first('body') }}</strong>
